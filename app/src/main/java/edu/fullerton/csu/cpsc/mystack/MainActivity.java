50d898c1005e1e86a46d1b2aa4ad04baf53fe376
Wen Qu <Wen@NUIT20458800ML.local>
2015-09-22 09:26:42 -0700

    added work for US19, the exit button is now functional

--- app/src/main/java/edu/fullerton/csu/cpsc/mystack/MainActivity.java
+++ app/src/main/java/edu/fullerton/csu/cpsc/mystack/MainActivity.java
@@ -60,6 +60,16 @@ public class MainActivity extends ActionBarActivity {
 
 
 
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
 
 
 }
